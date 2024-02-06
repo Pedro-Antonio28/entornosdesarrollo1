de34e365fb52a2f0831661ce12f26280444a3896
Pedro Antonio <1919952@alu.murciaeduca.es>
2024-02-06 19:14:44 +0100

    Actualizar para Practica4_1

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal{
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+        System.out.println("Hoy es 06/02/2024 y son las 18:00");
+
+    }
+}
